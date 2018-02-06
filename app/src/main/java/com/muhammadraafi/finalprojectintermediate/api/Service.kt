970b8f15bb6de59b065e901936b9436add12455a
emrafi <mr.firmanssyah@gmail.com>
2018-02-06 16:29:23 +0700

    First Commit

---
+++ app/src/main/java/com/muhammadraafi/finalprojectintermediate/api/Service.kt
@@ -0,0 +1,15 @@
+package com.muhammadraafi.finalprojectintermediate.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import com.muhammadraafi.finalprojectintermediate.model.MovieResponse
+/**
+ * Created by uty1 on 03/02/2018.
+ */
+interface Service {
+    @GET("movie/popular?api_key=e2cc92ec8be1b8f4ed1fbb3a66883d7f")
+    fun getMoviePopuler(): Call<MovieResponse>
+
+    @GET("movie/top_rated?api_key=e2cc92ec8be1b8f4ed1fbb3a66883d7f")
+    fun getMovieTopRated() : Call<MovieResponse>
+}
